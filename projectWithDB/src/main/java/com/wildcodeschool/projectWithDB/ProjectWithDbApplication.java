58451857b1054847abda7b9b2f495f92647731bc
Arthur Jouanneau <jouanneauarthur@gmail.com>
2019-06-04 17:56:00 +0200

    Request to bdd

---
+++ projectWithDB/src/main/java/com/wildcodeschool/projectWithDB/ProjectWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.projectWithDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithDbApplication.class, args);
+	}
+
+}
